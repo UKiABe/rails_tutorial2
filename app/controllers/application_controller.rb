278fd79af3d8726e10228615557200d44f8ac87a
UkiAbe <unlimited.blade.works22@gmail.com>
2018-10-30 08:50:13 +0900

    Use as a Toy-app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "¡Hola"
-  end
-
   def goodbye
     render html: "goodbye"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   root 'application#goodbye'
-  get 'user', to: 'application#hello'
 end
